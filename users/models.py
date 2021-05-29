0798d8c5531a85085b7222916df81acf55169d9f
omkar <omkarjadhav880@gmail.com>
2021-05-29 23:08:57 +0530

    new requirements

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from PIL import Image
 # Create your models here.
 class profile(models.Model):
 	user=models.OneToOneField(User,on_delete=models.CASCADE)
-	img=models.ImageField(default="default.jpg",upload_to="profile_pics") 
+	img=models.ImageField(default="default.png",upload_to="profile_pics") 
 
 	def __str__(self):
 		return f"{self.user.username} profile"
